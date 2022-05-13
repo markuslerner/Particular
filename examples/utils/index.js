c96dea08b25037e8cbfed19194a56faca3d54172
Markus Lerner <m@markuslerner.com>
2022-05-13 09:34:40 +0200

    Simplify code

--- examples/utils/index.js
+++ examples/utils/index.js
@@ -1,7 +1,9 @@
+import { Vector3 } from 'three';
+
 export const random = (min = 0, max = 1) => Math.random() * (max - min) + min;
 
-export const updateMousePosition = (camera, vec, pos) => (event) => {
-  vec.set(
+export const updateMousePosition = (camera, pos) => (event) => {
+  const vec = new Vector3(
     (event.clientX / window.innerWidth) * 2 - 1,
     -(event.clientY / window.innerHeight) * 2 + 1,
     0.5
@@ -16,18 +18,18 @@ export const updateMousePosition = (camera, vec, pos) => (event) => {
   pos.copy(camera.position).add(vec.multiplyScalar(distance));
 };
 
-export const addMouseListener = (renderer, camera, vec, pos) => {
+export const addMouseListener = (renderer, camera, pos) => {
   const supportsPointerEvents = !!window.PointerEvent;
 
   if (supportsPointerEvents) {
     renderer.domElement.ownerDocument.addEventListener(
       'pointermove',
-      updateMousePosition(camera, vec, pos)
+      updateMousePosition(camera, pos)
     );
   } else {
     renderer.domElement.ownerDocument.addEventListener(
       'mousemove',
-      updateMousePosition(camera, vec, pos)
+      updateMousePosition(camera, pos)
     );
   }
 };
