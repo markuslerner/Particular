792fa68b284b5a4fd88987567c5925ac0ffad3f9
Markus Lerner <m@markuslerner.com>
2022-10-13 18:24:54 +0200

    Add distance calculation example

--- src/physics/GPUPhysics.js
+++ src/physics/GPUPhysics.js
@@ -1,20 +1,50 @@
 import { GPU } from 'gpu.js';
-const gpu = new GPU();
 
-// const sqrt = gpu
-//   .createKernel(function (a) {
-//     return Math.sqrt(a);
+// const length = gpu
+//   .createKernel(function (x, y, z) {
+//     return Math.sqrt(x * x + y * y + z * z);
 //   })
-//   .setOutput([1]);
-
-const length = gpu
-  .createKernel(function (x, y, z) {
-    return Math.sqrt(x * x + y * y + z * z);
-  })
-  .setOutput([2]);
-console.log(length(2, 2, 2));
+//   .setOutput([2]);
+// console.log(length(2, 2, 2));
 
 import SimplePhysics from './SimplePhysics.js';
+
+function distance(a, b) {
+  let dx = a[0] - b[0];
+  let dy = a[1] - b[1];
+  let dz = a[2] - b[2];
+  return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+let elements = [
+  [1, 1, 0],
+  [2, 2, 2],
+  [3, 3, 3],
+  [4, 4, 4],
+  // ... 100k elements
+];
+
+const gpu = new GPU({ mode: 'cpu' });
+gpu.addFunction(distance, {
+  argumentTypes: {
+    a: 'Array(3)',
+    b: 'Array(3)',
+  },
+  returnType: 'Float',
+});
+
+const kernelMap = gpu.createKernel(
+  function kernelFunction(elems) {
+    return distance(elems[this.thread.x], elems[this.thread.y]);
+  },
+  {
+    dynamicArguments: true,
+    output: [elements.length, elements.length],
+  }
+);
+
+const result = kernelMap(elements);
+
+console.log(result);
 
 export default class GPUPhysics extends SimplePhysics {
   constructor(props) {
@@ -24,6 +54,19 @@ export default class GPUPhysics extends SimplePhysics {
     super(props);
 
     this.buffersize = buffersize;
+
+    // const calculateDistances = gpu
+    //   .createKernel(function (a, b) {
+    //     // let sum = 0;
+    //     // for (let i = 0; i < buffersize; i++) {
+    //     //   sum += a[this.thread.y][i] * b[i][this.thread.x];
+    //     // }
+    //
+    //     return a[this.thread.y] + '_' + b[this.thread.x];
+    //   })
+    //   .setOutput([buffersize, buffersize]);
+
+    // calculateDistances
   }
 
   updateParticles(deltaTime) {
